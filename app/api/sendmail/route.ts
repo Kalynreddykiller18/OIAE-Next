db2c32cc6adca1287e5a989dd366fad7c90d9a6a
Kalynreddykiller18 <kalyanbejjanki9010@gmail.com>
2024-07-11 23:46:19 +0530

    UI fixes - about page

--- app/api/sendmail/route.ts
+++ app/api/sendmail/route.ts
@@ -18,7 +18,7 @@ export async function POST(request: Request){
 
     const mailOptions = {
         from:process.env.USER,
-        to:process.env.OWNER,
+        to:"accounts@oiaes.com",
         subject:`Customer concern - ${maildata.subject}`,
         text:maildata.text
     }
