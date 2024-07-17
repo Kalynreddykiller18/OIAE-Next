0a96ff2dc577a739f5835def0b03939ce4c59961
Kalynreddykiller18 <kalyanbejjanki9010@gmail.com>
2024-07-17 11:21:52 +0530

    cors added 1.0

--- app/api/query/route.ts
+++ app/api/query/route.ts
@@ -9,6 +9,19 @@ if (!uri) {
   throw new Error('Please define the MONGO_URI environment variable inside .env.local');
 }
 
+export const dynamic = 'force-dynamic' // defaults to auto
+ 
+export async function GET(request: Request) {
+  return new Response('Hello, Next.js!', {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  })
+}
+
 export async function POST(request: Request) {
   try {
     const userQuerie = await request.json();
--- app/api/sendmail/route.ts
+++ app/api/sendmail/route.ts
@@ -1,5 +1,18 @@
 const nodemailer = require('nodemailer');
 
+export const dynamic = 'force-dynamic' // defaults to auto
+ 
+export async function GET(request: Request) {
+  return new Response('Hello, Next.js!', {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  })
+}
+
 export async function POST(request: Request){
 
     const maildata = await request.json();
