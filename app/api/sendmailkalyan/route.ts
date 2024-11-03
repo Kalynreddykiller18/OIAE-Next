b934091e69217b2752eb2eefdd427f2ffd55287d
Kalyan Reddy Bejjanki <kalyanreddybejjanki@Kalyans-MacBook-Air.local>
2024-11-03 20:10:54 +0530

    cors3

--- app/api/sendmailkalyan/route.ts
+++ app/api/sendmailkalyan/route.ts
@@ -46,7 +46,7 @@ export async function POST(request: Request) {
     return new Response(JSON.stringify({ message: 'Email sent successfully' }), {
       headers: {
         "Content-Type": "application/json",
-        'Access-Control-Allow-Origin': 'https://kalyanreddybejjanki.netlify.app/',
+        'Access-Control-Allow-Origin': 'https://kalyanreddybejjanki.netlify.app',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
       },
@@ -58,7 +58,7 @@ export async function POST(request: Request) {
     return new Response(JSON.stringify({ message: 'Error in mail sending' }), {
       headers: {
         "Content-Type": "application/json",
-        'Access-Control-Allow-Origin': 'https://kalyanreddybejjanki.netlify.app/',
+        'Access-Control-Allow-Origin': 'https://kalyanreddybejjanki.netlify.app',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
       },
@@ -70,7 +70,7 @@ export async function POST(request: Request) {
 export async function OPTIONS(request: Request) {
   return new Response(null, {
     headers: {
-      'Access-Control-Allow-Origin': 'https://kalyanreddybejjanki.netlify.app/',
+      'Access-Control-Allow-Origin': 'https://kalyanreddybejjanki.netlify.app',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     },
